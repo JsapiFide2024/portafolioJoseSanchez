27f21dffdac8903b2608bd132e60c11603247c92
JsapiFide2024 <jsanchez30543@ufide.ac.cr>
2024-01-24 18:33:59 -0600

    Creacion de proyecto

---
+++ tienda_JoseS/src/main/java/tienda/demo/TiendaJoseSApplication.java
@@ -0,0 +1,13 @@
+package tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaJoseSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaJoseSApplication.class, args);
+	}
+
+}
---
+++ tienda_JoseS/src/test/java/tienda/demo/TiendaJoseSApplicationTests.java
@@ -0,0 +1,13 @@
+package tienda.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaJoseSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
